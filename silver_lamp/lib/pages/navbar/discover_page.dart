f8921d96f0386f943d3dd0691a43133e8d5ce359
Benvenutas <benvenutas.grinius@northumbria.ac.uk>
2023-05-01 21:00:27 +0100

    Select video functionality

--- silver_lamp/lib/pages/navbar/discover_page.dart
+++ silver_lamp/lib/pages/navbar/discover_page.dart
@@ -1,33 +1,99 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import 'dart:ffi';
+import 'dart:io';
 
-class DiscoverPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+class DiscoverPage extends StatefulWidget {
   const DiscoverPage({Key? key}) : super(key: key);
 
   @override
+  _DiscoverPageState createState() => _DiscoverPageState();
+}
+
+class _DiscoverPageState extends State<DiscoverPage> {
+  File? _videoFile;
+
+  final ImagePicker _picker = ImagePicker();
+  late VideoPlayerController _controller;
+  final _snackBarDisplayDuration = Duration(seconds: 2);
+  bool _initialized = false;
+
+  Future<void> _pickVideo() async {
+    final pickedFile = await _picker.pickVideo(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _videoFile = File(pickedFile.path);
+      });
+      _controller = VideoPlayerController.file(_videoFile!);
+      _controller.addListener(() {
+        if (_controller.value.isPlaying) {
+          setState(() {});
+        }
+      });
+      await _controller.initialize();
+      _initialized = true;
+      setState(() {});
+    }
+  }
+
+  Future<void> _uploadVideo() async {
+    if (_initialized) {
+      // TODO: Implement backend upload logic
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Video uploaded"),
+          duration: _snackBarDisplayDuration,
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser!;
-    return Center(
-        child: Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text('signed in as: ' + user.email!),
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              color: Colors.deepPurple[200],
-              child: Text('sign out'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Discover"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (_videoFile == null)
+            Center(
+              child: Text("No video selected"),
+            )
+          else if (_initialized)
+            Center(
+                child: Container(
+              height: 200,
+              child: AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              ),
+            ))
+          else
+            Center(
+              child: CircularProgressIndicator(),
             ),
-          ],
-        ),
-        Text(
-          'Discover',
-          style: TextStyle(fontSize: 50),
-        ),
-      ],
-    ));
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _pickVideo,
+            child: Text("Select video"),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _uploadVideo,
+            child: Text("Upload video"),
+          ),
+        ],
+      ),
+    );
   }
 }
