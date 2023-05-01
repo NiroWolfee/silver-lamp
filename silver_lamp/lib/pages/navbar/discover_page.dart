acb0a33702b0d1d2f041543651e51992de790181
Benvenutas <benvenutas.grinius@northumbria.ac.uk>
2023-05-01 22:08:38 +0100

    fixed video player issues

--- silver_lamp/lib/pages/navbar/discover_page.dart
+++ silver_lamp/lib/pages/navbar/discover_page.dart
@@ -1,9 +1,93 @@
-import 'dart:ffi';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player/video_player.dart';
+
+class VideoPlayerWidget extends StatefulWidget {
+  final File videoFile;
+
+  const VideoPlayerWidget({Key? key, required this.videoFile})
+      : super(key: key);
+
+  @override
+  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  late VideoPlayerController _controller;
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.file(widget.videoFile);
+    _controller.initialize().then((_) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.pause();
+    _controller.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant VideoPlayerWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.videoFile != oldWidget.videoFile) {
+      _controller.pause();
+      _controller.dispose();
+      _controller = VideoPlayerController.file(widget.videoFile);
+      _controller.initialize().then((_) {
+        setState(() {});
+      });
+    }
+  }
+
+  void _togglePlay() {
+    if (_isPlaying) {
+      _controller.pause();
+    } else {
+      _controller.play();
+    }
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _togglePlay,
+      child: _controller.value.isInitialized
+          ? AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  VideoPlayer(_controller),
+                  AnimatedOpacity(
+                    opacity: _isPlaying ? 0 : 1,
+                    duration: Duration(milliseconds: 500),
+                    child: Container(
+                      color: Colors.black.withOpacity(0.5),
+                      child: Center(
+                        child: Icon(Icons.play_arrow, size: 64),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+}
 
 class DiscoverPage extends StatefulWidget {
   const DiscoverPage({Key? key}) : super(key: key);
@@ -16,9 +100,6 @@ class _DiscoverPageState extends State<DiscoverPage> {
   File? _videoFile;
 
   final ImagePicker _picker = ImagePicker();
-  late VideoPlayerController _controller;
-  final _snackBarDisplayDuration = Duration(seconds: 2);
-  bool _initialized = false;
 
   Future<void> _pickVideo() async {
     final pickedFile = await _picker.pickVideo(source: ImageSource.gallery);
@@ -26,34 +107,17 @@ class _DiscoverPageState extends State<DiscoverPage> {
       setState(() {
         _videoFile = File(pickedFile.path);
       });
-      _controller = VideoPlayerController.file(_videoFile!);
-      _controller.addListener(() {
-        if (_controller.value.isPlaying) {
-          setState(() {});
-        }
-      });
-      await _controller.initialize();
-      _initialized = true;
-      setState(() {});
     }
   }
 
   Future<void> _uploadVideo() async {
-    if (_initialized) {
-      // TODO: Implement backend upload logic
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Video uploaded"),
-          duration: _snackBarDisplayDuration,
-        ),
-      );
+    if (_videoFile == null) {
+      return;
     }
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
+    // TODO: Implement backend upload logic
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Video uploaded")),
+    );
   }
 
   @override
@@ -69,23 +133,16 @@ class _DiscoverPageState extends State<DiscoverPage> {
             Center(
               child: Text("No video selected"),
             )
-          else if (_initialized)
-            Center(
-                child: Container(
-              height: 200,
-              child: AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              ),
-            ))
           else
             Center(
-              child: CircularProgressIndicator(),
-            ),
+                child: Container(
+              height: 300,
+              child: VideoPlayerWidget(videoFile: _videoFile!),
+            )),
           SizedBox(height: 20),
           ElevatedButton(
             onPressed: _pickVideo,
-            child: Text("Select video"),
+            child: Text("Pick a video"),
           ),
           SizedBox(height: 20),
           ElevatedButton(
--- silver_lamp/lib/pages/navbar/search_page.dart
+++ silver_lamp/lib/pages/navbar/search_page.dart
@@ -6,9 +6,35 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text(
-      'Search',
-      style: TextStyle(fontSize: 50),
-    ));
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'User Manual',
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Text(
+            '1. Record a video of you exercising',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 10),
+          Text(
+            '2. Crop the video to contain only exercise motion',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 10),
+          Text(
+            '3. Upload the cropped video to the app',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 10),
+          Text(
+            '4. Wait for your exercise report',
+            style: TextStyle(fontSize: 20),
+          ),
+        ],
+      ),
+    );
   }
 }
